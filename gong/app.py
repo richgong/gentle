ce78a9b5fb556c9637e6ae03c9fcc40eaf8e6240
Richard Gong <richard.gong@instacart.com>
2019-11-02 23:58:12 -0700

    templates

--- gong/app.py
+++ gong/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_view():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about_view():
+    return render_template('about.html')
