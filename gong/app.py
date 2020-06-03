f61e258dd5dca37b0388c4f76ae064c0e17654de
Richard Gong <richardgong@gmail.com>
2020-06-02 23:29:57 -0700

    get working again

--- gong/app.py
+++ gong/app.py
@@ -1,3 +1,13 @@
+from os.path import dirname, realpath
+import sys
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+PROJECT_PATH = dirname(realpath(__file__))
+add_path(PROJECT_PATH)
+add_path(dirname(PROJECT_PATH))
+
+
 import time
 import logging
 from flask import Flask, render_template, jsonify, send_from_directory, current_app, Markup
